5c00106be34073c9960c4da82c5f2c97a72f317f
julien59155 <51157220+julien59155@users.noreply.github.com>
2019-06-20 13:36:35 +0200

    Update WebSecurityConfig.java

--- demo/src/main/java/com/kf/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/kf/demo/config/WebSecurityConfig.java
@@ -18,8 +18,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .csrf()
                 .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/ajouterjeu").authenticated()
                 .and()
